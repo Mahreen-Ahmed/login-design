fe3b59b23aea2fd8a69fed3f1e190463f46d7335
Mahreen-Ahmed <88930659+Mahreen-Ahmed@users.noreply.github.com>
2022-03-02 19:59:59 -0800

    complete

---
+++ js/jt.js
@@ -0,0 +1,43 @@
+//console.log("Hello world");
+//var a; a=2; alert(a);
+/*let x="Hello";
+let y="World";
+console.log(x+" "+"  "+y);*/
+
+/*let x=5;
+let y="5";
+//x++;
+console.log(++x);*/
+
+
+/*let x=5;
+x="hello";
+console.log(x);*/
+ //console.log('"hel\"lo"');
+
+
+ /*let x=['Ali',"Hisham","Kiki"];// array
+ let man={name:'ALI', age:"hisham"};// object;*/
+
+ //${}uses
+ /*let x="Mehreen";
+ console.log(`hello ${x}`);*/
+
+
+//if condition
+// alert() used to display a messgae
+//prompt() used to show a messgae on scree and also ask the user for input, if return text text click ok otherwise click cancel or esc;
+//confirm() show a message with confirm it consider true for okay and flase for cancel;
+//alert("Welcome to icode guru!");
+
+
+let age= prompt('Enter your age ' , 20);
+
+if(age!= null){
+    document.write('Your Age is ' + age );
+
+}
+else{
+    document.write("Your canceled the prompt");
+}
+
